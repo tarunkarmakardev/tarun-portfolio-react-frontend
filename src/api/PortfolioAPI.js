b5aafefb8f99b9263fcff53da5aa9a2b9b74d223
Tarun <tarun.karmakar.dev@gmail.com>
2021-04-23 11:06:19 +0530

    updated env

--- src/api/PortfolioAPI.js
+++ src/api/PortfolioAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+const { REACT_APP_BACKEND_URL } = process.env;
 
 export default axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: REACT_APP_BACKEND_URL,
 });
